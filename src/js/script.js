6691a32846307398cffd21bdd20878aa022aee0b
Jesus Antonio Lopez Bandala <lopezbandalajesusantonio@gmail.com>
2025-03-16 23:42:42 -0600

    Versión 3.0: Botones de guardado de variable

--- src/js/script.js
+++ src/js/script.js
@@ -2,16 +2,29 @@ const $inputPantalla = document.getElementById("input-pantalla");
 const SYNTAXERROR = "SYNTAX ERROR";
 const INFINITY = "Infinity";
 const NAN = "NaN";
+const UNDEFINED = "Undefined";
+let numAlmacenado = null;
 let historialTemp = []; //[[Operación, Resultado]]
 
 const isErrorText = (str) =>
-  str === SYNTAXERROR || str === INFINITY || str === NAN;
+  str === SYNTAXERROR || str === INFINITY || str === NAN || str === UNDEFINED;
 
 const insertClickedClass = (button) => {
   button.classList.add("clicked");
   setTimeout(() => {
     button.classList.remove("clicked");
   }, 100);
+};
+
+const clearDecimales = ($input) => {
+  if (
+    $input.textContent[$input.textContent.length - 1] === "0" &&
+    $input.textContent[$input.textContent.length - 2] === "0"
+  ) {
+    let tempText = $input.textContent.split("");
+    for (let i = 0; i < 3; i++) tempText.pop();
+    $input.textContent = tempText.join("");
+  }
 };
 
 // Impresión de pantalla de los caracteres
@@ -75,17 +88,7 @@ document.getElementById("btn-igual").addEventListener("click", function () {
     const resultado = math.evaluate(expresion);
     if (resultado.toString() !== INFINITY && resultado.toString() !== NAN) {
       $inputPantalla.textContent = Number(resultado).toFixed(2).toString();
-      if (
-        $inputPantalla.textContent[$inputPantalla.textContent.length - 1] ===
-          "0" &&
-        $inputPantalla.textContent[$inputPantalla.textContent.length - 2] ===
-          "0"
-      ) {
-        let tempText = $inputPantalla.textContent.split("");
-        for (let i = 0; i < 3; i++) tempText.pop();
-        $inputPantalla.textContent = tempText.join("");
-      }
-
+      clearDecimales($inputPantalla);
       expresion = expresion.replace("*", "×").replace("/", "÷");
       historialTemp.push([expresion, $inputPantalla.textContent]);
       return;
@@ -177,3 +180,54 @@ document
       .splice(0, lastIndexOperation)
       .join("");
   });
+
+// Actualización del input del valor guardado
+const $inputValorGuardado = document.getElementById("input-valor-guardado");
+const refreshInputValorGuardado = () => {
+  $inputValorGuardado.textContent =
+    numAlmacenado === null ? UNDEFINED : numAlmacenado.toString();
+  clearDecimales($inputValorGuardado);
+};
+refreshInputValorGuardado();
+
+const regexNumber = /^\-{0,1}[0-9]{1,}(\.{1}[0-9]{1,}|)$/;
+
+// Botón MC
+document.getElementById("btn-mc").addEventListener("click", function () {
+  insertClickedClass(this);
+  numAlmacenado = null;
+  refreshInputValorGuardado();
+});
+
+// Botón MR
+document.getElementById("btn-mr").addEventListener("click", function () {
+  insertClickedClass(this);
+  $inputPantalla.textContent =
+    numAlmacenado === null ? UNDEFINED : numAlmacenado.toString();
+  clearDecimales($inputPantalla);
+  refreshInputValorGuardado();
+});
+
+// Botón M+
+document.getElementById("btn-m+").addEventListener("click", function () {
+  insertClickedClass(this);
+  if (regexNumber.test($inputPantalla.textContent)) {
+    if (numAlmacenado === null)
+      numAlmacenado = Number($inputPantalla.textContent);
+    else
+      numAlmacenado =
+        Number(numAlmacenado) + Number($inputPantalla.textContent);
+    numAlmacenado = Number(numAlmacenado).toFixed(2);
+  }
+  refreshInputValorGuardado();
+});
+
+// Botón M-
+document.getElementById("btn-m-").addEventListener("click", function () {
+  insertClickedClass(this);
+  if (regexNumber.test($inputPantalla.textContent) && numAlmacenado !== null) {
+    numAlmacenado = Number(numAlmacenado) - Number($inputPantalla.textContent);
+    numAlmacenado = Number(numAlmacenado).toFixed(2);
+  }
+  refreshInputValorGuardado();
+});
